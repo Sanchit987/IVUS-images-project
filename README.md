dc31351eb535e03a1abc886a19e99c7d9e4eb8c9
Raghav Bansal <raghavbansal292@gmail.com>
2021-07-25 18:49:47 +0530

    Updated Readme.md with some final changes in code

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 
 1. Open terminal and type `pip install -r requirements.txt`
+2. `Sample Real Images` directory contains some sample dicom images on which further preprocessing is done.
+3. `apply_filter.py` will create the necessary filters by transforming images from `Sample Real Images` directory and save filters inside `IVUS Project` directory.
+4. `calculate_scores.ipynb` is a jupyter notebook which will calculate evaluation metrics such as PSNR, MSE, SSIM and save it into an excel sheet `data.xlsx` and csv file `data.csv`.  
+
+
+> Python 3.7
--- apply_filter.py
+++ apply_filter.py
@@ -16,11 +16,12 @@ import os
 
 
 
-os.chdir('IVUS Project')
+#os.chdir('IVUS Project')
 files = []
 for file in os.listdir("Sample Real Images"):
     if file.endswith(".dcm"):
         files.append(file)
+
 
 NUMBER_OF_IMAGES = len(files)
 for i in range(NUMBER_OF_IMAGES):
@@ -30,15 +31,15 @@ for i in range(NUMBER_OF_IMAGES):
 
     """ Converting RGB Image to Grayscale image and saving it """
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(os.path.join("Grayscale Images/", files[i][:-4] + ".png"), grayscale)
+    cv2.imwrite(os.path.join("IVUS Project/Grayscale Images/", files[i][:-4] + ".png"), grayscale)
 
     """ Applying Anisotropic Diffusion Filter and saving it """
     img_anisotropic = anisotropic_diffusion(grayscale)
-    cv2.imwrite(os.path.join("Anisotropic Filter Images/", files[i][:-4] + ".png"), img_anisotropic)
+    cv2.imwrite(os.path.join("IVUS Project/Anisotropic Filter Images/", files[i][:-4] + ".png"), img_anisotropic)
 
     """ Applying wiener filter and saving it """
     img_wiener = wiener(grayscale)
-    cv2.imwrite(os.path.join("Wiener Filter Images/", files[i][:-4] + ".png"), img_wiener)
+    cv2.imwrite(os.path.join("IVUS Project/Wiener Filter Images/", files[i][:-4] + ".png"), img_wiener)
 
     """ Applying NonLocal Means filter and saving it """
     sigma_est = np.mean(estimate_sigma(grayscale, multichannel=False))
@@ -49,12 +50,12 @@ for i in range(NUMBER_OF_IMAGES):
                               patch_distance=3,
                               preserve_range=True,
                               multichannel=False)
-    cv2.imwrite(os.path.join("Non-Local Means Filter Images/", files[i][:-4] + ".png"), img_ncm)
+    cv2.imwrite(os.path.join("IVUS Project/Non-Local Means Filter Images/", files[i][:-4] + ".png"), img_ncm)
 
     """ Applying median filter and saving it """
     # Disk creates a circular structuring element, similar to a mask with specific radius
     img_median = median(grayscale, disk(3), mode='constant', cval=0.0)
-    cv2.imwrite(os.path.join("Median Filter Images/", files[i][:-4] + ".png"), img_median)
+    cv2.imwrite(os.path.join("IVUS Project/Median Filter Images/", files[i][:-4] + ".png"), img_median)
 
     """ Applying bilateral filter and saving it """
     img_bilateral = denoise_bilateral(grayscale,
@@ -64,26 +65,26 @@ for i in range(NUMBER_OF_IMAGES):
     # sigma_color = float - Sigma for grey or color value.
     # For large sigma_color values the filter becomes closer to gaussian blur.
     # sigma_spatial: float. Standard ev. for range distance. Increasing this smooths larger features.
-    cv2.imwrite(os.path.join("Bilateral Filter Images/", files[i][:-4] + ".png"), img_bilateral)
+    cv2.imwrite(os.path.join("IVUS Project/Bilateral Filter Images/", files[i][:-4] + ".png"), img_bilateral)
 
     """ Applying gaussian filter and saving it """
     img_gaussian = gaussian(grayscale, sigma=1, mode='constant', cval=0.0)
     # sigma defines the std dev of the gaussian kernel.
-    cv2.imwrite(os.path.join("Gaussian Filter Images/", files[i][:-4] + ".png"), img_gaussian)
+    cv2.imwrite(os.path.join("IVUS Project/Gaussian Filter Images/", files[i][:-4] + ".png"), img_gaussian)
 
     """ Applying total variance filter and saving it """
     img_denoise = denoise_tv_chambolle(grayscale, weight=0.1, eps=0.0002, n_iter_max=200, multichannel=False)
-    cv2.imwrite(os.path.join("Total Variance Filter Images/", files[i][:-4] + ".png"), img_denoise)
+    cv2.imwrite(os.path.join("IVUS Project/Total Variance Filter Images/", files[i][:-4] + ".png"), img_denoise)
 
 
     """ Applying hybrid filters """
     # 1. Wiener + Anisotropic filter
     img_wiener_anisotropic = anisotropic_diffusion(img_wiener)
-    cv2.imwrite(os.path.join("Wiener + Anisotropic Filter Images/", files[i][:-4] + ".png"), img_wiener_anisotropic)
+    cv2.imwrite(os.path.join("IVUS Project/Wiener + Anisotropic Filter Images/", files[i][:-4] + ".png"), img_wiener_anisotropic)
 
     # 2. Non-local Means filter + Median filter
     img_ncm_median = median(img_ncm, disk(3), mode='constant', cval=0.0)
-    cv2.imwrite(os.path.join("Non-local Means filter + Median Filter Images/", files[i][:-4] + ".png"), img_ncm_median)
+    cv2.imwrite(os.path.join("IVUS Project/Non-local Means filter + Median Filter Images/", files[i][:-4] + ".png"), img_ncm_median)
 
 
 
